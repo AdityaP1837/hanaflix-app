1375b99522dec21c0222a724d3f752c7702be0d4
Aditya <mrknightkhonshu34@gmail.com>
2023-12-08 18:37:40 +0530

    Flask Added with Templates, etc

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template("home.html")
+
+@app.route('/trending/<time>/<page>')
+def trending(time, page):
+    return render_template("trending.html", time=time, page=page)
+
+@app.route('/browse')
+def browse():
+    return render_template("browse.html")
+
+@app.route('/watch/<slug>')
+def watch(slug):
+    return render_template("watch.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
